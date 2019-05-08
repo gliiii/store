6f1ef31992b2f04a49a6778dcb5b36b57defaf94
glii0 <lig08190@gmail.com>
2019-05-08 15:20:34 +0800

    拉去提交

--- src/main/java/cn/tedu/store/Hello.java
+++ src/main/java/cn/tedu/store/Hello.java
@@ -4,10 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 	System.out.println("left...");
 	System.out.println("hello");
-<<<<<<< HEAD
 	System.out.println("1111");
-=======
->>>>>>> branch 'master' of https://github.com/gliiii/store.git
 	System.out.println("2222");
 }
 }
