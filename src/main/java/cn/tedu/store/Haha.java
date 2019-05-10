5b82fca04bc2c309cdb6960d2a4d425f8ef8579e
glii0 <lig08190@gmail.com>
2019-05-10 09:32:13 +0800

    test assume

--- src/main/java/cn/tedu/store/Haha.java
+++ src/main/java/cn/tedu/store/Haha.java
@@ -4,6 +4,7 @@ public class Haha {
 public static void main(String[] args) {
 	System.out.println("this is a new file");
 	System.out.println("1111");
+	System.out.println("test assume...");
 }
 }
  
