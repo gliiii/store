d11ad0c553aca09d885ed120f8c173c8e88aa12c
glii0 <lig08190@gmail.com>
2019-05-10 10:36:12 +0800

    this is a dev branch

--- src/main/java/cn/tedu/store/Haha.java
+++ src/main/java/cn/tedu/store/Haha.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	System.out.println("this is a new file");
 	System.out.println("1111");
 	System.out.println("test assume...");
+	System.out.println("this is a dev branch");
 }
 }
  
