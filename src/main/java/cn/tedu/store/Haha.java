f660afe2219f32d79c3a08ea987332e663271e4c
glii0 <lig08190@gmail.com>
2019-05-14 09:58:25 +0800

    test stashes

--- src/main/java/cn/tedu/store/Haha.java
+++ src/main/java/cn/tedu/store/Haha.java
@@ -6,6 +6,7 @@ public static void main(String[] args) {
 	System.out.println("1111");
 	System.out.println("test assume...");
 	System.out.println("this is a dev branch");
+	System.out.println("heiheihei");
 }
 }
  
