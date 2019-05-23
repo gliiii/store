8fe4633a4dad07cf6d0d459a6fcc0954854d424e
glii0 <lig08190@gmail.com>
2019-05-23 17:20:31 +0800

    fetch test
    Change-Id: Ie4f3621ebd1377c6ad0e74856147268ccca45ce9
    Signed-off-by: glii0 <lig08190@gmail.com>

--- src/main/java/cn/tedu/store/Haha.java
+++ src/main/java/cn/tedu/store/Haha.java
@@ -10,6 +10,7 @@ public static void main(String[] args) {
 	System.out.println("222");
 	System.out.println("333");
 	System.err.println("444");
+	System.out.println("this is a fetch test");
 }
 }
  
