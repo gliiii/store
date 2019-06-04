af0d35e13428d13ba66d7faad5641ddeed0261c5
glii0 <lig08190@gmail.com>
2019-06-04 10:07:53 +0800

    qqq

--- src/main/java/cn/tedu/store/Hello.java
+++ src/main/java/cn/tedu/store/Hello.java
@@ -4,15 +4,5 @@ public class Hello {
 	public static void main(String[] args) {
 	System.out.println("left...");
 	System.out.println("hello");
-	System.out.println("1111");
-	System.out.println("2222");
-	System.out.println("4444");
-	System.out.println("5555");
-	System.out.println("hahahaa");
-	System.out.println("222");
-	System.out.println("444");
-	System.out.println("777");
-	System.err.println("888");
-	System.out.println("ddddqq"); 
 }
 }
