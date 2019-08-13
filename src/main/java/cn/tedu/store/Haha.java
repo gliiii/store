15af9efa9a70f9238158e9d2f43e7c6ce0e4a949
glii0 <lig08190@gmail.com>
2019-08-13 10:46:31 +0800

    555

--- src/main/java/cn/tedu/store/Haha.java
+++ src/main/java/cn/tedu/store/Haha.java
@@ -27,6 +27,8 @@ public static void main(String[] args) {
 	System.out.println("111");
 	System.out.println("333");
 	System.out.println("222");
+	System.out.println("555");
+	System.out.println("444");
 }
 }
  
