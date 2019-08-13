bc94493486571fcb3bfe5b9372470834e2dcc310
glii0 <lig08190@gmail.com>
2019-08-13 11:19:51 +0800

    fff

--- src/main/java/cn/tedu/store/Haha.java
+++ src/main/java/cn/tedu/store/Haha.java
@@ -36,6 +36,8 @@ public static void main(String[] args) {
 	System.out.println("aaa");
 	System.out.println("bbb");
 	System.out.println("ccc");
+	System.out.println("fff");
+	System.out.println("ddd");
 }
 }
  
