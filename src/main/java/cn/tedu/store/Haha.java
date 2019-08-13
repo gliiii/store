4b3e042a5d675a96c08f6f911ca96005166a4ed7
glii0 <lig08190@gmail.com>
2019-08-13 19:06:39 +0800

    revert

--- src/main/java/cn/tedu/store/Haha.java
+++ src/main/java/cn/tedu/store/Haha.java
@@ -3,8 +3,6 @@ package cn.tedu.store;
 public class Haha {
 public static void main(String[] args) {
 	System.out.println("test cancel");
-	System.out.println("cancel2");
-	System.out.println("revert3");
 }
 }
  
