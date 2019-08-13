a544bf0ea0454d93c20968156656d9914810ed30
glii0 <lig08190@gmail.com>
2019-08-13 11:23:53 +0800

    hhh

--- src/main/java/cn/tedu/store/Haha.java
+++ src/main/java/cn/tedu/store/Haha.java
@@ -36,7 +36,10 @@ public static void main(String[] args) {
 	System.out.println("aaa");
 	System.out.println("bbb");
 	System.out.println("ccc");
-	System.out.println("ddd");
+	System.out.println("fff");
+	System.out.println("ddd"); 
+	System.out.println("eee");
+	System.out.println("hhh");
 }
 }
  
