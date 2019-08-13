cfadb795dae20bab4918642420480357cac65488
glii0 <lig08190@gmail.com>
2019-08-13 18:48:05 +0800

    test cancel

--- src/main/java/cn/tedu/store/Haha.java
+++ src/main/java/cn/tedu/store/Haha.java
@@ -2,44 +2,7 @@ package cn.tedu.store;
 
 public class Haha {
 public static void main(String[] args) {
-	System.out.println("this is a new file");
-	System.out.println("1111");
-	System.out.println("test assume...");
-	System.out.println("this is a dev branch");
-	System.out.println("heiheihei");
-	System.out.println("222");
-	System.out.println("333");
-	System.err.println("444");
-	System.out.println("this is a fetch test");
-	System.out.println("fetch 2");
-	System.out.println("dirty mode");
-	System.out.println("heiheihei");
-	System.out.println("test3");
-	System.out.println("revert1");
-	System.out.println("revert2");
-	System.out.println("show history");
-	System.out.println("history1");
-	System.out.println("workspace");
-	System.out.println("hsitory222");
-	System.out.println("show history");
-	System.out.println("history1");
-	System.out.println("sychronize workspacce");
-	System.out.println("111");
-	System.out.println("333");
-	System.out.println("222"); 
-	System.out.println("555");
-	System.out.println("444");
-	System.out.println("666");
-	System.out.println("777");
-	System.out.println("999");
-	System.out.println("888");
-	System.out.println("aaa");
-	System.out.println("bbb");
-	System.out.println("ccc");
-	System.out.println("fff");
-	System.out.println("ddd"); 
-	System.out.println("eee");
-	System.out.println("hhh"); 
+	System.out.println("test cancel");
 }
 }
  
