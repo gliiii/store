cb8b4a2064e335d30619ccb72b4a555f628dada4
glii0 <lig08190@gmail.com>
2019-08-14 15:04:40 +0800

    333

--- src/main/java/cn/tedu/store/Haha.java
+++ src/main/java/cn/tedu/store/Haha.java
@@ -4,6 +4,7 @@ public class Haha {
 public static void main(String[] args) {
 	System.out.println("test cancel");
 	System.out.println(System.getProperties().getProperty("file.separator"));
+	System.out.println("333");
 }
 }
  
