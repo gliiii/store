0cec2a37b16fca956869d8cf924f1d6d92b9ac33
glii0 <lig08190@gmail.com>
2019-08-14 14:58:42 +0800

    1111

--- src/main/java/cn/tedu/store/Haha.java
+++ src/main/java/cn/tedu/store/Haha.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	System.out.println("test cancel");
 	System.out.println("cancel2");
 	System.out.println("revert3");
+	System.out.println(System.getProperties().getProperty("file.separator"));
 }
 }
  
