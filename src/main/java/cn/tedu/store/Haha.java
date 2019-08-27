8da75e8b17465d1a00953ea5fe7821cb20b2e9e2
glii0 <lig08190@gmail.com>
2019-08-27 19:59:39 +0800

    aaa

--- src/main/java/cn/tedu/store/Haha.java
+++ src/main/java/cn/tedu/store/Haha.java
@@ -2,25 +2,7 @@ package cn.tedu.store;
 
 public class Haha {
 public static void main(String[] args) {
-	System.out.println("test cancel");
-	System.out.println(System.getProperties().getProperty("file.separator"));
-	System.out.println("111");
-	System.out.println("222");
-	System.out.println("444");
-	System.out.println("666");
-	System.out.println("555");
-	System.out.println("777");
-	System.out.println("444");
-
-	System.out.println("eee");
-	System.out.println("qaaaaaa");
-	System.out.println("111");
-	System.out.println("2222");
-	System.out.println("333");
-	System.out.println("555");
-	System.out.println("444");
-	System.out.println("666");
-	System.out.println("777");
+	System.out.println("aaa");
 }
 }
  
