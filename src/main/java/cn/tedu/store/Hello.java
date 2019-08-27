b2ac85a0b2f323b331d79d223b14bc7e63c2fae0
glii0 <lig08190@gmail.com>
2019-08-27 19:58:48 +0800

    111

--- src/main/java/cn/tedu/store/Hello.java
+++ src/main/java/cn/tedu/store/Hello.java
@@ -2,29 +2,6 @@ package cn.tedu.store;
 
 public class Hello {
 	public static void main(String[] args) {
-	System.out.println("left...");
-	System.out.println("hello");
-	System.out.println("1111");
-	System.out.println("2222");
-	System.out.println("4444");
-	System.out.println("5555");
-	System.out.println("hahahaa");
-	System.out.println("222");
-	System.out.println("444");
-	System.out.println("777");
-	System.err.println("888");
-	System.out.println("ddddqq"); 
-	System.out.println("test reset!");
-	System.out.println("rest1");
-	System.out.println("rest2");
-	
-	System.out.println("origin");
-	System.out.println("test2");
-	System.out.println("test444");
-	System.err.println("local testt");
-	System.err.println("test push commit!");
-	System.out.println("2222");
-	System.out.println("333");
-	System.out.println("ddd");
-}
+		System.out.println("111");
+	}
 }
