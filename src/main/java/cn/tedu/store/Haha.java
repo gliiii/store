99e200cf0c739c80e04cba9b766e825040acde90
glii0 <lig08190@gmail.com>
2019-09-25 16:27:35 +0800

    test assume unchanged

--- src/main/java/cn/tedu/store/Haha.java
+++ src/main/java/cn/tedu/store/Haha.java
@@ -15,6 +15,7 @@ public static void main(String[] args) {
 	System.out.println("test");
 	System.out.println("test overwrite");
 	System.out.println("test 222");
+	System.out.println("test assume unchanged");
 }
 }
  
