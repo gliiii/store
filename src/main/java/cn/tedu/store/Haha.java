124e274c82897965d93902c9593ab10b0bd9781f
glii0 <lig08190@gmail.com>
2019-10-17 10:18:30 +0800

    6

--- src/main/java/cn/tedu/store/Haha.java
+++ src/main/java/cn/tedu/store/Haha.java
@@ -19,11 +19,7 @@ public static void main(String[] args) {
 	System.out.println("test 333");
 	System.out.println("666");
 	System.out.println("test conflict");
-	System.out.println("1");
-	System.out.println("2");
-	System.out.println("3");
-	System.out.println("4");
-	System.out.println("5");
+	System.out.println("6");
 }
 }
  
