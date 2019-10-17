95a7f0974658de6d98850f57e6b28d44f2f4f6a7
glii0 <lig08190@gmail.com>
2019-10-17 10:29:43 +0800

    clear

--- src/main/java/cn/tedu/store/Haha.java
+++ src/main/java/cn/tedu/store/Haha.java
@@ -2,26 +2,6 @@ package cn.tedu.store;
 
 public class Haha {
 public static void main(String[] args) {
-	System.out.println("aaa");
-	System.out.println("bbb");
-	System.out.println("ccc");
-	System.out.println("1111");
-	System.out.println("111");
-	System.out.println("dev conflict");
-	System.out.println("delete fix");
-	System.out.println("delete feature");
-	System.out.println("test conflict");
-	System.out.println("test delete");
-	System.out.println("test");
-	System.out.println("test overwrite");
-	System.out.println("test 222");
-	System.out.println("test assume unchanged");
-	System.out.println("test 333");
-	System.out.println("666");
-	System.out.println("test conflict");
-	System.out.println("6");
-	System.out.println("a");
-	System.out.println("b");
 }
 }
  
--- src/main/java/cn/tedu/store/Hello.java
+++ src/main/java/cn/tedu/store/Hello.java
@@ -2,15 +2,5 @@ package cn.tedu.store;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("111");
-		System.out.println("222");
-		System.out.println("333");
-		System.out.println("444");
-		System.out.println("555");
-		System.out.println("test conflict2");
-		System.out.println("test conflict3");
-		System.out.println("aa");
-		System.out.println("bb");
-		System.out.println("cc");
 	}
 }
