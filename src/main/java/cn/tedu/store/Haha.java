c24d66b45344dd86ccb789cdb1a5cdf6bec0298f
glii0 <lig08190@gmail.com>
2019-10-17 10:42:30 +0800

    clear

--- src/main/java/cn/tedu/store/Haha.java
+++ src/main/java/cn/tedu/store/Haha.java
@@ -2,7 +2,6 @@ package cn.tedu.store;
 
 public class Haha {
 public static void main(String[] args) {
-	System.out.println("3");
 }
 }
  
--- src/main/java/cn/tedu/store/Hello.java
+++ src/main/java/cn/tedu/store/Hello.java
@@ -2,6 +2,5 @@ package cn.tedu.store;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("c");
 	}
 }
