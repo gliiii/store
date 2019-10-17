ac03a781829b1e620a4bb06213de1d17409e2875
glii0 <lig08190@gmail.com>
2019-10-17 10:37:36 +0800

    c 3

--- src/main/java/cn/tedu/store/Haha.java
+++ src/main/java/cn/tedu/store/Haha.java
@@ -2,8 +2,7 @@ package cn.tedu.store;
 
 public class Haha {
 public static void main(String[] args) {
-	System.out.println("1");
-	System.out.println("2");
+	System.out.println("3");
 }
 }
  
--- src/main/java/cn/tedu/store/Hello.java
+++ src/main/java/cn/tedu/store/Hello.java
@@ -2,7 +2,6 @@ package cn.tedu.store;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("a");
-		System.out.println("b");
+		System.out.println("c");
 	}
 }
