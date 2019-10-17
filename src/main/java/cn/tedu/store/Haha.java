4c31575ffbab1c0837b64414670e1ada9cb18d49
glii0 <lig08190@gmail.com>
2019-10-17 11:31:49 +0800

    Revert "aaa"
    This reverts commit 16ed101605a017b0a19bc4943d10126d65485f62.
    
    Conflicts:
    	src/main/java/cn/tedu/store/Haha.java

--- src/main/java/cn/tedu/store/Haha.java
+++ src/main/java/cn/tedu/store/Haha.java
@@ -2,12 +2,7 @@ package cn.tedu.store;
 
 public class Haha {
 public static void main(String[] args) {
-	System.out.println("aaa");
-	System.out.println("bbb");
-	System.out.println("ccc");
-	System.out.println("1111");
-	System.out.println("111");
-	System.out.println("dev conflict");
+	System.out.println("1");
 }
 }
  
